6865e9b89fd2438b1e1662a09c72426b216a4210
jban-as <banas.joven@gmail.com>
2023-02-19 16:16:59 +0000

    Deploying to gh-pages from @ jban-as/jban-as.github.io@69381d872f111162d27a503f168984a7a6e196a0 🚀

--- server/chunks/app/_nuxt/default-styles.f6107ee5.mjs
+++
@@ -1,6 +0,0 @@
-const default_vue_vue_type_style_index_0_lang = "body{background-color:#fff;color:rgba(0,0,0,.8)}.dark-mode body{background-color:#091a28;color:#ebf4f1}.sepia-mode body{background-color:#f1e7d0;color:#433422}.app-container main{margin:0 auto;padding:0 1rem;width:900px}";
-
-const defaultStyles_f6107ee5 = [default_vue_vue_type_style_index_0_lang];
-
-export { defaultStyles_f6107ee5 as default };
-//# sourceMappingURL=default-styles.f6107ee5.mjs.map
--- server/chunks/app/_nuxt/index-styles.8892316b.mjs
+++
@@ -1,6 +0,0 @@
-const index_vue_vue_type_style_index_0_scoped_62b2e90d_lang = ".about-me-section[data-v-62b2e90d]{padding-top:10rem;transition:opacity .25s ease 0s}";
-
-const indexStyles_8892316b = [index_vue_vue_type_style_index_0_scoped_62b2e90d_lang];
-
-export { indexStyles_8892316b as default };
-//# sourceMappingURL=index-styles.8892316b.mjs.map
